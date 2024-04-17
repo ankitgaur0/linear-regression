834f0e77376f9655ea162c0e72cc810b3a3f9d31
ankitgaur0 <ankitparashar000@gmail.com>
2024-04-17 14:10:55 +0530

    this is the template in which i write the code to create the structure of project which means to create the folder and files of the project by using os library to automatic creatation

---
+++ template.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+
+import logging
+
+package_name="diamond price prediction"
+
+list_of_files=[
+    "github/workflows/.gitkeep",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/components/__init__.py",
+    f"src/{package_name}/components/Data_Injestion.py",
+    f"src/{package_name}/components/Data_preprocessing.py",
+    f"src/{package_name}/components/Model_traning.py",
+    f"src/{package_name}/Pipeline/__init__.py",
+    f"src/{package_name}/Pipeline/training_pipeline.py",
+    f"src/{package_name}/pipeline/prediction_pipeline.py",
+    f"src/{package_name}/Exceptionhandle.py",
+    f"src/{package_name}/Logger.py",
+    f"src/{package_name}/Util.py",
+    "notebook/Research.ipynb",
+    "notebook/data/.gitkeep",
+    "init_setup.py",
+    "setup.py"
+]
+
+for filepath in list_of_files:
+    filepath=Path(filepath)
+
+    filedir,filename=os.path.split(filepath)
+
+    if(filedir !=""):
+        os.makedirs(filedir,exist_ok=True)
+
+    if (not os.path.exists(filepath)) or(os.path.getsize(filepath)==0):
+        with open(filepath,"w") as f:
+            pass
+    
+    else:
+        print("file is already exists!")
+
+
